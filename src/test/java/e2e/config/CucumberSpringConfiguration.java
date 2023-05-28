03d2f435022538b282d6f6cf993654d1474286dc
”YevgenyDyachenko” <yevgeny.dyachenko@gmail.com>
2023-05-28 22:39:01 +0300

    version 2.5

--- src/test/java/e2e/config/CucumberSpringConfiguration.java
+++ src/test/java/e2e/config/CucumberSpringConfiguration.java
@@ -1,10 +1,10 @@
 package e2e.config;
 
-import com.ua.hotel.RobotSpringDemoApplication;
+import com.ua.hotel.HotelApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = RobotSpringDemoApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = HotelApplication.class)
 public class CucumberSpringConfiguration {
 }
